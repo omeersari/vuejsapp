e7257f0e2f33e2f05eca0d03a2757caa51ed483b
omeersari <mryellow95@gmail.com>
2020-08-30 16:05:48 +0300

    detail-vcont

--- filmrafi/src/views/Detail.vue
+++ filmrafi/src/views/Detail.vue
@@ -1,11 +1,29 @@
 <template>
   <div class="head">
   <div class="about" :style="`background-image: url(https://image.tmdb.org/t/p/original/${detailMovie.backdrop_path})`"> </div>
-    <h1 class="title">{{detailMovie.title}}</h1>
-    <div class="movie">
-      <img class="poster" :src="`https://image.tmdb.org/t/p/original/${detailMovie.poster_path}`" />
+    <div class="title">
+      <h1>{{detailMovie.title}}</h1>
+      <small v-for="(genres,id) in detailMovie.genres" :key="id"> {{genres.name}} | </small>
     </div>
+    <div class="movie">
+      <div class="left">
+        <img class="poster" :src="`https://image.tmdb.org/t/p/original/${detailMovie.poster_path}`" />
+      </div>
+        <div class="right">
+          <small > Release Date: {{detailMovie.release_date}} </small>
+          <small > Duration: {{detailMovie.runtime}} min</small>
+          <span class="imdb">
+            <img src="../assets/imdb.png" style="width: 75px; height: auto" > <p> {{detailMovie.vote_average}}</p>
+          </span>
+          <h4 style="font-style: italic; margin-top: 15px;"> {{detailMovie.tagline}} </h4>
+          <h1> Overview</h1>
+          <p>{{detailMovie.overview}}</p>
 
+
+        </div>
+
+
+    </div>
   </div>
 </template>
 
@@ -26,6 +44,8 @@ export default {
   position: relative;
   z-index: 1;
   background-color: blue;
+  height: 500px;
+
 }
 .head .about {
   background-repeat: no-repeat;
@@ -39,7 +59,7 @@ export default {
   right: 0;
   opacity: 0.6;
   width: 100%;
-  height: 100%;
+  height: 500px;
 }
 
 .title {
@@ -48,11 +68,40 @@ export default {
 }
 
 .movie {
-  margin-left: 35px;
+  margin-left: 15px;
+  color: white;
 }
+
+.right {
+  margin-left: 300px;
+}
+
+.left{
+  float: left;
+  clear: both;
+}
+
+
 .poster {
   width: 200px;
   height: 300px;
+}
+
+.imdb {
+  display: flex;
+  justify-content: flex-start;
+}
+
+.imdb p {
+  background-color: black;
+  border: 0.5px solid darkblue;
+  border-radius: 50%;
+  width: 48px;
+  height: 48px;
+  text-align: center;
+  padding-top: 10px;
+  color: gold;
+  margin-left: 15px;
 }
 
 
