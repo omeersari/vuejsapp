62acea1f267bc2a5e70adbc662d29a128d861684
omeersari <mryellow95@gmail.com>
2020-09-24 17:34:49 +0300

    morleng start

---
+++ morleng/src/components/Login.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="login">
+    <a @click="goBack">Go back</a>
+    <div class="formRegister">
+      <h1>{{ isLogin ? "Sign in" : "Sign up" }}</h1>
+      <form action="">
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+      </form>
+      <button type="submit">{{ isLogin ? "SIGN IN" : "SIGN UP" }}</button>
+      <p>
+        {{ isLogin ? "Don't have an account ? " : "Already have an account ?" }}
+        <a @click="changeForm"> {{ isLogin ? "sign up" : "sign in" }}.</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      isLogin: false
+    };
+  },
+  methods: {
+    changeForm() {
+      this.isLogin = !this.isLogin;
+    },
+    goBack() {
+      this.$emit('goBack', false)
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login {
+
+}
+
+.formRegister {
+  color: white;
+  font-family: "Courgette", cursive;
+  padding: 50px;
+}
+
+.formRegister form {
+  display: flex;
+  flex-direction: column;
+  padding: 30px 30px 15px 30px;
+}
+
+.formRegister form input {
+  height: 30px;
+  padding: 0 20px;
+  border: 2px solid rgb(219, 219, 219);
+  margin-bottom: 20px;
+  width: 190px;
+  border-radius: 3px;
+}
+
+.formRegister button {
+  padding: 10px;
+  background-color: rgba(143, 173, 185, 0.1);
+  color: white;
+  border: 1px solid white;
+  width: 90px;
+  transition: 300ms all;
+  border-radius: 3px;
+}
+
+.formRegister button:hover {
+  background-color: black;
+  cursor: pointer;
+}
+
+p {
+  color: white;
+  font-family: "Courgette", cursive;
+}
+
+a {
+  color: white;
+  text-decoration: underline;
+  cursor: pointer;
+  font-family: "Courgette", cursive;
+
+}
+</style>
---
+++ morleng/src/views/Home/Home.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="home">
+    <div class="main" v-if="!getStarted">
+      <h1>Morleng</h1>
+      <p>There are more languages in the world than these stars you see now.</p>
+      <h5>Why don't you start learning one of them right now ?</h5>
+      <button @click="goToLogin">Get Started</button>
+    </div>
+    <small class="by">
+      <a href="https://www.instagram.com/_shotinraw/"
+        >Photo by Juan from Pexels</a
+      ></small
+    >
+    <Login v-if="getStarted" @goBack="comeBack" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from "@/components/Login";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      getStarted: false
+    }
+  },
+  components: {
+    Login,
+  },
+  methods: {
+    goToLogin() {
+      this.getStarted = true
+    },
+    comeBack() {
+      this.getStarted = false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.home {
+  height: 100vh;
+  background-image: url("../../assets/background.jpg");
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.by {
+  position: absolute;
+  bottom: 0px;
+  right: 0px;
+}
+.by a {
+  text-decoration: none;
+  color: white;
+  transition: 300ms all;
+}
+
+.by a:hover {
+  text-decoration: underline;
+}
+
+.main {
+  color: white;
+}
+
+.main h1 {
+  margin-top: 0px;
+  margin-bottom: 50px;
+  text-transform: uppercase;
+  font-family: "Courgette", cursive;
+}
+
+.main p {
+  font-size: 22px;
+  font-weight: bold;
+  font-family: "Courgette", cursive;
+}
+
+.main h5 {
+  font-family: "Courgette", cursive;
+}
+
+button {
+  margin-top: 15px;
+  padding: 1rem;
+  border: 2px solid white;
+  border-radius: 3px;
+  color: white;
+  text-transform: uppercase;
+  background-color: rgba(143, 173, 185, 0.1);
+  font-weight: bold;
+  transition: 300ms all;
+  font-family: "Courgette", cursive;
+}
+
+button:hover {
+  background-color: black;
+  cursor: pointer;
+}
+</style>
